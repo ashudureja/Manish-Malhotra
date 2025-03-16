da3922652be6007279f0588916eb539e413962e0
ashudureja <ashutosh.dureja512@gmail.com>
2025-03-16 23:18:17 +1100

    Your commit message

--- src/Components/Work2.jsx
+++ src/Components/Work2.jsx
@@ -52,7 +52,7 @@ const MediaContainer = ({
             initial={{ opacity: 1 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.1 }}
           />
         )}
       </AnimatePresence>
@@ -70,7 +70,7 @@ const MediaContainer = ({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.1 }}
           />
         )}
       </AnimatePresence>
